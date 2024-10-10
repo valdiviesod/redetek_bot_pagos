42d9752d6c7db68f26f5cfa394a017585d79a937
Your Name <you@example.com>
2024-10-10 11:12:29 -0500

    Reconexion + linea

--- base-js-baileys-json/src/app.js
+++ base-js-baileys-json/src/app.js
@@ -12,8 +12,8 @@ const welcomeFlow = addKeyword(['Hola', 'Buenas', 'Buenos', 'Buen', 'Saludos', '
     .addAnswer(`Si deseas realizar un pago de manera física, escribe *fisico* para obtener información sobre cómo realizar tus pagos en nuestros puntos físicos.`)
     .addAnswer(`Si necesitas la dirección de nuestras oficinas, escribe *oficinas*.`)
     .addAnswer(`Si deseas realizar un pago de manera virtual, escribe *virtual* para comenzar con tu proceso de pago en línea.`)
-    .addAnswer(`Si necesitas soporte técnico, escribe *soporte* para obtener la línea de soporte técnico.`)
-
+    .addAnswer(`Si necesitas realizar una *Reconexión del servicio* o soporte técnico en general, escribe *soporte* para obtener la línea de soporte técnico.`)
+    .addAnswer(['Si necesitas información sobre planes o realizar la *cancelación* del servicio, comunícate vía Whatsapp a la línea:  3182530010'])
 
 const fisicoFlow = addKeyword(['Físico', 'físico', 'Fisico', 'fisico'])
     .addAnswer('De manera presencial manejamos *pagos en efectivo* con horario en nuestras oficinas de 08:00 am a 05:00 pm de lunes a sábado. No aplica festivos ni domingos.')
